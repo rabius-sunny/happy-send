e640497d12d211f78aa3a87c012b4ecc7a649a3c
rabius-sunny <rabiussunnyc3x@gmail.com>
2024-05-10 02:27:02 +0600

    add: basic error handling

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const path = require('path')
 const cookieParser = require('cookie-parser')
+const { notFoundHandler, errorHandler } = require('./middlewares/errorHandler')
 
 const PORT = process.env.PORT || 3000
+
+const app = express()
+dotenv.config()
 
 // database connection
 
@@ -22,6 +26,10 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(cookieParser(process.env.COOKIE_SECRET))
 
 // routings
+
+// handle errors
+app.use(notFoundHandler)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`)
